6b716cf8c038da606aa3607ef1fc8c8cdf1d999d
twinkll <sisodia.t@husky.neu.edu>
2020-08-24 00:00:46 -0400

    Add runner.py file and authorised

---
+++ runner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#
+#          FILE:  runner.py
+#
+#
+#   DESCRIPTION:  Manage rules
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#        AUTHOR:  Twinkll Sisodia
+#       COMPANY:  Red Hat
+#       VERSION:  1.0
+#       CREATED:  08/21/2020
+# ===============================================================================
