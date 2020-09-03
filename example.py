83c03b6cd4a0a01cebe0e15ca230aa83d110c02c
twinkll <sisodia.t@husky.neu.edu>
2020-09-02 20:51:33 -0400

    Example code

---
+++ example.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+import subprocess
+import sys
+import threading
+import time
+
+parser = argparse.ArgumentParser(description='Manage rules')
+
+parser.add_argument('-c', '--count', type=int,
+                     action='store', 
+                     required=True, dest='count')
+
+parser.add_argument('-fc', '--failed-count', type=int,
+                     action='store', 
+                     required=True, dest='failed')
+
+parser.add_argument('-m', '--mode', 
+                     action='store', choices={'debug','help'}, dest='mode')
+
+
+args = parser.parse_args()
+
+
+import time
+
+import threading
+
+
+class TestThreading(object):
+
+ # --- check count and also checks the returncode - if it's success or failure 
+ def __init__(self, interval=1):
+
+      self.interval = interval
+
+      thread = threading.Thread(target=self.run, args=())
+      thread.start()
+
+
+ def run(self):
+      num = 0
+      # --- check and execute ping command with -c 
+      while num < args.count:
+        
+      
+          print("CALLING 1st process")
+          process = subprocess.Popen(["ping","-c 2", "google.com"], 
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)  
+            # output, errors = process.communicate()
+          process.poll()
+
+          self.stdout, self.stderr = process.communicate()
+          print(self.stdout)
+          print(self.stderr)
+               
+          def netTracing():
+             print("Calling NETSTAT process")
+             netTrace = subprocess.Popen(["netstat", "ping", "-at", "google.com"], 
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+             time.sleep(2)
+             output, errors = netTrace.communicate()
+             print(output)
+             print(errors)
+             netTrace.poll()
+
+          def sysTracing():
+             print("Calling SYSTRACE process")   
+             sysTrace = subprocess.Popen(["strace", "-c", "ping", "google.com"], 
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+             output, errors = sysTrace.communicate()
+             sysTrace.poll()     
+             print(output)
+             print(errors)              
+             time.sleep(2)
+             print("SYSTRACE RUN FINISHED")   
+
+       
+          a = threading.Thread(target=netTracing, name='Thread-a')
+          print("NOW RUNNING NETSTAT")
+          a.start()
+          a.join()
+          b = threading.Thread(target=sysTracing, name='Thread-b')
+          print("NOW RUNNING SYSTRACE")
+          b.start()
+          b.join()
+          
+
+                        
+
+tr = TestThreading()
