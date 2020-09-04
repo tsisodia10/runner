4e6f83b51ecf4b2bc9e793d4b4c9d9a160df822a
twinkll <sisodia.t@husky.neu.edu>
2020-09-04 15:08:51 -0400

    Update business logic

--- runner.py
+++ runner.py
@@ -19,123 +19,101 @@ import argparse
 import os
 import subprocess
 import sys
-
+import threading
+import time
+import shlex
 
 parser = argparse.ArgumentParser(description='Manage rules')
+
+parser.add_argument('-com', '--command',
+                     action='store', 
+                     required=True, dest='command')
 
 parser.add_argument('-c', '--count', type=int,
                      action='store', 
                      required=True, dest='count')
 
-parser.add_argument('-fc', '--failed-count', type=int,
-                     action='store', 
-                     required=True, dest='failed')
-
 parser.add_argument('-m', '--mode', 
                      action='store', choices={'debug','help'}, dest='mode')
+
 
 args = parser.parse_args()
 
 
-# --- check count and also checks the returncode - if it's success or failure 
-def check_count():
-      num = 0
-      # --- check and execute ping command with -c 
-      while num < args.count:
+class TestThreading(object):
+
+ # --- check count and also checks the returncode - if it's success or failure 
+ def __init__(self, interval=1):
+
+      self.interval = interval
+
+      thread = threading.Thread(target=self.run, args=())
+      thread.start()
+      thread.join()
+
+
+ def run(self, timeout=0):
+        num = 0
         
-          process = subprocess.run('ping -c 2 google.com', shell=True,
+        # --- check and execute ping command with -c   
+        while num < args.count: 
+          print("CALLING 1st process")
+          process = subprocess.Popen(shlex.split(args.command), 
                                    stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)      
-          print('Returncode: ', process.returncode)  
-          print('Have {} bytes in stdout:\n{}'.format(
-                len(process.stdout),
-                process.stdout.decode('utf-8'))
-          )    
-          print('Have {} bytes in stderr: {!r}'.format(
-                len(process.stderr),
-                process.stderr.decode('utf-8'))
-          )              
+                                   stderr=subprocess.PIPE)
+          output, errors = process.communicate()                           
+          
+          print(output)
+          print(errors)  
+          process.poll()
+          proc = process.pid
+          # str_proc = str(proc)
+          print("Printing pid", proc) 
+          num = num + 1                   
+          time.sleep(0.2)
 
-        
-          num = num + 1
-          if process.returncode == 0:
-             print("Success")
-             # --- Success tracing
-             success_tracing()
-          else: 
-             error_tracing()
-
-
-# --- function which displays tracing for successful execution
-def success_tracing():
-
-      print("\n\nTRACING SUCCESSFUL EXECUTION -------------------------------------\n")
-
-      # --- execute ping and parse the result
-      pingParsing = os.system(f'pingparsing google.com -c {args.count}')
-      print("\n\nMEMORY AND DISK USAGE --------------------------------------------\n")
-
-      # --- system calls 
-      sysTrace = os.system(f'strace -c ping google.com')
-
-      print(sysTrace)
-      print(pingParsing)
-
-      # --- debug mode
-      if args.mode == 'debug':
-          print("DEBUGGING MODE")
-          debug=os.system(f'ping -c {args.count} -d google.com')
-          print(debug)
-
-      # --- help mode
-      if args.mode == 'help':
-          print("MANUAL")
-          help=os.system(f'ping -h')     
-          print(help) 
-      
-      
-# --- function whih displays tracing for failed execution      
-def error_tracing():
-      
-      # --- numc - variable flag
-      numc = 0
-      
-      # --- while failed count is less than 2, it will execute till condition become false
-      while numc < args.failed:
-            process = subprocess.run('ping -c 2 google.com', shell=True,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE) 
-            numc = numc + 1   
-
-      print("Returncode : \n",process.returncode)   
-      print("TRACING MEMORY USAGE OF FAILED EXECUTION--------------------------------\n")
-      sysTrace = os.system(f'strace ping -c {args.count} google.com')
-      print("TRACING FAILED EXECUTION------------------------------------------------\n")
-      pingParsing = os.system(f'pingparsing google.com -c {args.count}')
-      netTrace = subprocess.run(['netstat','ping','-s','google.com'],
+          if process.returncode == 0:                         
+            print("Successful execution")
+          else:
+            def netTracing():
+             command = str(args.command)
+             print("Calling NETSTAT process")
+             netTrace = subprocess.Popen(["netstat", "-at", " | ", "grep", str(proc)],  
                                 stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                universal_newlines=True)
-      print(sysTrace)
-      print(pingParsing)
-      print(netTrace)
-      print("Use the debug mode to trace errors : usage : ping -d")
-      
-      # --- debug mode
-      if args.mode == 'debug':
-          print("DEBUGGING MODE")
-          debug=os.system(f'ping -c {args.count} -d google.com')
-          print(debug)
+                                stderr=subprocess.PIPE)
+            #  print(netTrace.stdout)
+            #  print(netTrace.stderr)
+             output, errors = netTrace.communicate()
+             print(output)
+             print(errors)
+             netTrace.poll()
+             time.sleep(0.2)
+            #  print(netTrace.stdout.readline())
 
-      # --- help mode
-      if args.mode == 'help':
-          print("MANUAL")
-          help=os.system(f'ping -h')     
-          print(help)
+            def sysTracing():
+             print("Calling SYSTRACE process")  
+             sysTrace = subprocess.Popen(["sudo","strace","-s", "80", "-fp", str(proc)],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
 
+             output, errors = sysTrace.communicate()
+             sysTrace.poll()  
+            #  print(sysTrace.stdout)
+            #  print(sysTrace.stderr)   
+             print(output)
+             print(errors) 
+             time.sleep(0.2)             
+             print("SYSTRACE RUN FINISHED")   
 
-# --- main function
-def main():
-        check_count()
-       
-main()
+           
+            a = threading.Thread(target=netTracing, name='Thread-a')
+            print("NOW RUNNING NETSTAT")
+            a.start()
+            a.join()
+            b = threading.Thread(target=sysTracing, name='Thread-b')
+            print("NOW RUNNING SYSTRACE")
+            b.start()
+            b.join()
+          
+               
+tr = TestThreading()
