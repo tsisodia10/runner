8646713d51eef356da89ea6b302e73dc8eb9e1d7
twinkll <sisodia.t@husky.neu.edu>
2020-09-03 21:10:53 -0400

    update

--- example.py
+++ example.py
@@ -1,19 +1,25 @@
+
 import argparse
 import os
 import subprocess
 import sys
 import threading
 import time
+import shlex
 
 parser = argparse.ArgumentParser(description='Manage rules')
+
+parser.add_argument('-com', '--command',
+                     action='store', 
+                     required=True, dest='command')
 
 parser.add_argument('-c', '--count', type=int,
                      action='store', 
                      required=True, dest='count')
 
-parser.add_argument('-fc', '--failed-count', type=int,
-                     action='store', 
-                     required=True, dest='failed')
+# parser.add_argument('-fc', '--failed-count', type=int,
+#                      action='store', 
+#                      required=True, dest='failed')
 
 parser.add_argument('-m', '--mode', 
                      action='store', choices={'debug','help'}, dest='mode')
@@ -31,60 +37,70 @@ class TestThreading(object):
 
       thread = threading.Thread(target=self.run, args=())
       thread.start()
+      thread.join()
 
 
- def run(self):
-      
-        def check_count():
+ def run(self, timeout=0):
+        num = 0
+          # --- check and execute ping command with -c   
+        while num < args.count: 
           print("CALLING 1st process")
-          process = subprocess.Popen(["ping","-c 2", "google.com"], 
+          process = subprocess.Popen(shlex.split(args.command), 
                                    stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)  
-            # output, errors = process.communicate()
+                                   stderr=subprocess.PIPE)
+          # output, errors = process.communicate()                           
+          returncode = process.returncode 
+          
+          # print(output)
+          # print(errors)  
           process.poll()
           proc = process.pid
-          str_proc = str(proc)
-          print("Printing pid", proc)
-          self.stdout, self.stderr = process.communicate()
-          print(self.stdout)
-          print(self.stderr)
-               
-          def netTracing():
+          # str_proc = str(proc)
+          print("Printing pid", proc) 
+          num = num + 1                   
+          time.sleep(0.2)
+          if returncode == 0:                         
+            print("Successful execution")
+          else:
+            def netTracing():
+             command = str(args.command)
              print("Calling NETSTAT process")
-             netTrace = subprocess.Popen(["netstat", "ping", "-at", "google.com"], 
+             netTrace = subprocess.Popen(["ps", "-p", str(proc)], shell=True, 
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-             time.sleep(2)
+            #  print(netTrace.stdout)
+            #  print(netTrace.stderr)
              output, errors = netTrace.communicate()
              print(output)
              print(errors)
              netTrace.poll()
+             time.sleep(0.2)
+            #  print(netTrace.stdout.readline())
 
-          def sysTracing():
-             print("Calling SYSTRACE process")   
-             sysTrace = subprocess.Popen(["strace", "-p", str_proc], 
+            def sysTracing():
+             print("Calling SYSTRACE process")  
+             sysTrace = subprocess.Popen(["sudo","strace", "-p", str(proc)],
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
 
              output, errors = sysTrace.communicate()
-             sysTrace.poll()     
+             sysTrace.poll()  
+            #  print(sysTrace.stdout)
+            #  print(sysTrace.stderr)   
              print(output)
-             print(errors)              
-             time.sleep(2)
+             print(errors) 
+             time.sleep(0.2)             
              print("SYSTRACE RUN FINISHED")   
 
-       
-          a = threading.Thread(target=netTracing, name='Thread-a')
-          print("NOW RUNNING NETSTAT")
-          a.start()
-          a.join()
-          b = threading.Thread(target=sysTracing, name='Thread-b')
-          print("NOW RUNNING SYSTRACE")
-          b.start()
-          b.join()
+            a = threading.Thread(target=netTracing, name='Thread-a')
+            print("NOW RUNNING NETSTAT")
+            a.start()
+            a.join()
+            b = threading.Thread(target=sysTracing, name='Thread-b')
+            print("NOW RUNNING SYSTRACE")
+            b.start()
+            b.join()
           
-        a1 = threading.Thread(target=check_count, name='Thread-a1')
-        a1.start()
-                        
-
+          
+                  
 tr = TestThreading()
